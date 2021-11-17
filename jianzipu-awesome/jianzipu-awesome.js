e86f1d5222a5c0a404ecc28ae97abdaa32cbde40
Nancy Yi Liang <hi@nyl.io>
2021-11-16 22:04:55 -0800

    add ability to keep brackets

--- jianzipu-awesome/jianzipu-awesome.js
+++ jianzipu-awesome/jianzipu-awesome.js
@@ -1,11 +1,15 @@
 console.log('Using [[JianZiPu]]')
 document.addEventListener("DOMContentLoaded", function(event) { 
 	var jzpClasses = document.getElementsByClassName("[[jzp]]")
-	for (var i=0;i<jzpClasses.length;i++) {
-		jzpClasses[i].innerHTML = jzpClasses[i].getInnerHTML().split('[[').join('<span class="jzp">').split(']]').join('</span>')
+	if (jzpClasses.length > 0 ){
+		for (var i=0;i<jzpClasses.length;i++) {
+			jzpClasses[i].innerHTML = jzpClasses[i].getInnerHTML().split('[[').join('<span class="jzp">').split(']]').join('</span>')
+		}
 	}
-	var jzpClassesKeep = document.getElementsByClassName("![[jzp]]")
-	for (var i=0;i<jzpClassesKeep.length;i++) {
-		jzpClassesKeep[i].innerHTML = jzpClasses[i].getInnerHTML().split('[[').join('[[<span class="jzp">').split(']]').join('</span>]]')
+	if (jzpClassesKeep.length > 0 ){
+		var jzpClassesKeep = document.getElementsByClassName("![[jzp]]")
+		for (var i=0;i<jzpClassesKeep.length;i++) {
+			jzpClassesKeep[i].innerHTML = jzpClasses[i].getInnerHTML().split('[[').join('[[<span class="jzp">').split(']]').join('</span>]]')
+		}
 	}
 });
