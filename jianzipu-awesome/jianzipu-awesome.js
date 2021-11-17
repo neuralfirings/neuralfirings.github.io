5b176cc668019db52dcd23988723bca07f017938
Nancy Yi Liang <hi@nyl.io>
2021-11-16 21:50:20 -0800

    javascript

--- jianzipu-awesome/jianzipu-awesome.js
+++ jianzipu-awesome/jianzipu-awesome.js
@@ -1,5 +1,7 @@
-console.log('using [[JianZiPu]]')
-var jzpClasses = document.getElementsByClassName("[[jzp]]")
-for (var i=0;i<jzpClasses.length;i++) {
-	jzpClasses[i].innerHTML = jzpClasses[i].getInnerHTML().split('[[').join('<span class="jzp">').split(']]').join('</span>')
-}
+console.log('Using [[JianZiPu]]')
+document.addEventListener("DOMContentLoaded", function(event) { 
+	var jzpClasses = document.getElementsByClassName("[[jzp]]")
+	for (var i=0;i<jzpClasses.length;i++) {
+		jzpClasses[i].innerHTML = jzpClasses[i].getInnerHTML().split('[[').join('<span class="jzp">').split(']]').join('</span>')
+	}
+});
